95a3367a597b91b5e41667b74c2f647ddb30aa6b
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-05 15:11:53 +0100

    correction

--- index.php
+++ index.php
@@ -1,21 +1,15 @@
 <?php
-$number = 3;
-/* déclaration d'une fonction "cube" qui va calculer le cube
- de la variable 'number' indiquée entre () pour ensuite retourner le résultat */
-function cube($number){
-  $calcul = $number * $number * $number;
-  return $calcul;
-}
-$result = cube($number);
+function test($par1, $par2){
+  return $par1 . $par2;
+};
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8" />
-    <title>PHP partie4 exo2</title>
+    <title>PHP partie 4 exo 2</title>
   </head>
   <body>
-    <p>Le résultat au cube de <?php echo $number; ?> est égal à <?php echo $result; ?></p>
-    <p><?php echo $number; ?> x <?php echo $number; ?> x <?php echo $number; ?> = <?php echo $result; ?></p>
+    <p><?php echo test('HE!', 'HO!') ?></p>
   </body>
 </html>
